5992c110617820f9e778c2e2f2fa5c9733987850
Stefan Djokic <stefan@one.fit>
2019-11-28 16:53:28 +0100

    Add more specific events

--- src/Models/Event.php
+++ src/Models/Event.php
@@ -10,4 +10,8 @@ abstract class Event
     public const EVENT_CREATED = 'created';
     public const EVENT_UPDATED = 'updated';
     public const EVENT_DELETED = 'deleted';
+    public const EVENT_FRIEND_INVITE_ACCEPTED = 'friend_invite_accepted';
+    public const EVENT_FRIEND_INVITE_DISMISSED = 'friend_invite_dismissed';
+    public const EVENT_WORKOUT_INVITE_ACCEPTED = 'workout_invite_accepted';
+    public const EVENT_WORKOUT_INVITE_DISMISSED = 'workout_invite_dismissed';
 }
