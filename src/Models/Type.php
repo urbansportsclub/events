7ec358521a258f020133d1b9be68e3065eb9ff60
Stefan Djokic <stefan@one.fit>
2020-01-03 13:42:09 +0100

    Add chargeable types

--- src/Models/Type.php
+++ src/Models/Type.php
@@ -10,4 +10,8 @@ abstract class Type
     public const NOTIFICATION = 'notification';
     public const MEMBER = 'member';
     public const PERIOD = 'period';
+    public const ORDERS = 'order';
+    public const RECURRING = 'recurring';
+    public const FINE = 'fine';
+    public const SURPLUS_MANDATE = 'surplus_mandate';
 }
