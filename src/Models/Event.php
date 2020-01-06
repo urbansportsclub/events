c881dbf0cf0f9f431c7e1ccfbf8e14b86795f049
Stefan Djokic <stefan@one.fit>
2020-01-06 11:25:08 +0100

    Add per chargeable event

--- src/Models/Event.php
+++ src/Models/Event.php
@@ -14,5 +14,8 @@ abstract class Event
     public const EVENT_FRIEND_INVITE_DISMISSED = 'friend_invite_dismissed';
     public const EVENT_WORKOUT_INVITE_ACCEPTED = 'workout_invite_accepted';
     public const EVENT_WORKOUT_INVITE_DISMISSED = 'workout_invite_dismissed';
-    public const EVENT_CHARGEABLE_STATUS_UPDATED = 'chargeable_status_updated';
+    public const EVENT_FINE_STATUS_UPDATED = 'fine_status_updated';
+    public const EVENT_ORDER_STATUS_UPDATED = 'order_status_updated';
+    public const EVENT_RECURRING_STATUS_UPDATED = 'recurring_status_updated';
+    public const EVENT_SURPLUS_MANDATE_STATUS_UPDATED = 'surplus_mandate_status_updated';
 }
