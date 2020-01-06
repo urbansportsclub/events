406b263ba654b765d9e8078eb162d3a076834e24
Stefan Djokic <stefan@one.fit>
2020-01-06 10:09:21 +0100

    Add chargeable status update event

--- src/Models/Event.php
+++ src/Models/Event.php
@@ -14,4 +14,5 @@ abstract class Event
     public const EVENT_FRIEND_INVITE_DISMISSED = 'friend_invite_dismissed';
     public const EVENT_WORKOUT_INVITE_ACCEPTED = 'workout_invite_accepted';
     public const EVENT_WORKOUT_INVITE_DISMISSED = 'workout_invite_dismissed';
+    public const EVENT_CHARGEABLE_STATUS_UPDATED = 'chargeable_status_updated';
 }
